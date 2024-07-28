7c3d78da407ec0d28f4182c1d3812cac9af1ca1f
Vijoy Sasidharan <mail@cybernetics.me>
2024-07-28 12:02:12 +0400

    Small updates

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -27,11 +27,11 @@ class WebController extends Controller
             ],
             ['captcha.captcha' => 'Invalid captcha code.']
         );
-        //try {
-        Mail::to('sales@maft.ae')->cc('cyberneticstvm@gmail.com')->send(new ContactFormSubmitEmail($request));
-        //} catch (Exception $e) {
-        //return redirect()->back()->with("error", $e->getMessage())->withInput($request->all());
-        //}
+        try {
+            Mail::to('sales@maft.ae')->cc('manzummm@gmail.com')->send(new ContactFormSubmitEmail($request));
+        } catch (Exception $e) {
+            return redirect()->back()->with("error", $e->getMessage())->withInput($request->all());
+        }
         return redirect()->back()->with("success", "Contact form submitted successfully!");
     }
 }

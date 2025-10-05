51bd553c8a3f62fb029a0a07c3e9f7e67b154c0f
Cybernetics <mail@cybernetics.me>
2025-10-05 16:41:42 +0530

    small updates

--- resources/views/mobile-nav.blade.php
+++ resources/views/mobile-nav.blade.php
@@ -1,4 +1,47 @@
 <div class="mobile-menu d-block d-xl-none">
+    <nav class="nav-main mainmenu-nav mt--30">
+        <ul class="mainmenu" id="mobile-menu-active">
+            <li>
+                <a href="#" class="main">Home</a>
+            </li>
+            <li>
+                <a class="main" href="{{ route('index') }}">About</a>
+            </li>
+            <li class="has-droupdown">
+                <a class="nav-link" href="#">Products</a>
+                <ul class="submenu inner-page">
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Snatch block & Tirfor</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Ratchet Load Binders</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Lever hoist</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Beam & Geared Trolley</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Cargo Lashing</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Round Sling</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Shackle</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Vertical & Horizontal plate clamp</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">LG-Chain block</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">5tonne Pallet truck</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Safe lift- magentic lifter</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">SAFE LIFT ELECTRIC CHAIN BLOCK 0.5 ton to 15 ton</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Lifting Gear-Catalogue </a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">chain-slings-LNS</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Wire rope sling</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Double webbing lanyard</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Full body harness</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Retractable fall arrester block</a></li>
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Anchorage life line rope</a></li>
+                </ul>
+            </li>
+            <li class="has-droupdown">
+                <a class="nav-link" href="#">Services</a>
+                <ul class="submenu inner-page">
+                    <li><a class="mobile-menu-link" href="{{ route('index') }}">Fabrication services</a></li>
+                </ul>
+            </li>
+            <li>
+                <a class="main" href="{{ route('contact') }}">Contact</a>
+            </li>
+        </ul>
+    </nav>
     <div class="row">
         <div class="col-md-4 single-contact-one-inner">
             <div class="content">
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,10 +1,43 @@
-<!--<nav>
+<nav>
     <ul>
         <li class="has-droupdown">
-            <a class="nav-link" href="{{ route('index') }}">Home</a>            
+            <a class="nav-link text-dark" href="{{ route('index') }}">Home</a>
         </li>
         <li class="has-droupdown">
-            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+            <a class="nav-link text-dark" href="{{ route('index') }}">About</a>
+        </li>
+        <li class="has-droupdown pages">
+            <a class="nav-link" href="#">Products</a>
+            <ul class="submenu inner-page">
+                <li><a href="{{ route('index') }}">LG-Snatch block & Tirfor</a></li>
+                <li><a href="{{ route('index') }}">Ratchet Load Binders</a></li>
+                <li><a href="{{ route('index') }}">LG-Lever hoist</a></li>
+                <li><a href="{{ route('index') }}">LG-Beam & Geared Trolley</a></li>
+                <li><a href="{{ route('index') }}">LG-Cargo Lashing</a></li>
+                <li><a href="{{ route('index') }}">LG-Round Sling</a></li>
+                <li><a href="{{ route('index') }}">LG-Shackle</a></li>
+                <li><a href="{{ route('index') }}">LG-Vertical & Horizontal plate clamp</a></li>
+                <li><a href="{{ route('index') }}">LG-Chain block</a></li>
+                <li><a href="{{ route('index') }}">5tonne Pallet truck</a></li>
+                <li><a href="{{ route('index') }}">Safe lift- magentic lifter</a></li>
+                <li><a href="{{ route('index') }}">SAFE LIFT ELECTRIC CHAIN BLOCK 0.5 ton to 15 ton</a></li>
+                <li><a href="{{ route('index') }}">Lifting Gear-Catalogue </a></li>
+                <li><a href="{{ route('index') }}">chain-slings-LNS</a></li>
+                <li><a href="{{ route('index') }}">Wire rope sling</a></li>
+                <li><a href="{{ route('index') }}">Double webbing lanyard</a></li>
+                <li><a href="{{ route('index') }}">Full body harness</a></li>
+                <li><a href="{{ route('index') }}">Retractable fall arrester block</a></li>
+                <li><a href="{{ route('index') }}">Anchorage life line rope</a></li>
+            </ul>
+        </li>
+        <li class="has-droupdown pages">
+            <a class="nav-link" href="#">Services</a>
+            <ul class="submenu inner-page">
+                <li><a href="{{ route('index') }}">Fabrication services</a></li>
+            </ul>
+        </li>
+        <li class="has-droupdown">
+            <a class="nav-item text-dark" href="{{ route('contact') }}">Contact</a>
         </li>
     </ul>
-</nav>-->
+</nav>

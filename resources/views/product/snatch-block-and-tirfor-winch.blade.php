b55bbc6a4733aff3832078dfb2feda29893eb9fe
Cybernetics <mail@cybernetics.me>
2025-10-07 11:24:59 +0530

    small updates

--- resources/views/product/snatch-block-and-tirfor-winch.blade.php
+++ resources/views/product/snatch-block-and-tirfor-winch.blade.php
@@ -7,11 +7,11 @@
                 <!-- bread crumb inner wrapper -->
                 <div class="breadcrumb-inner text-center breadcrumb-inner-bg">
                     <h1 class="text-white">{{ $title }}</h1>
-                    <div class="meta">
-                        <a href="{{ route('index') }}" class="prev text-white">Home/</a>
-                        <a href="{{ route('products') }}" class="prev text-white">products/</a>
-                        <a href="{{ route('product1') }}" class="next text-white">project Details</a>
-                    </div>
+                </div>
+                <div class="meta">
+                    <a href="{{ route('index') }}" class="prev text-white">Home/</a>
+                    <a href="{{ route('products') }}" class="prev text-white">products/</a>
+                    <a href="{{ route('product1') }}" class="next text-white">project Details</a>
                 </div>
                 <!-- bread crumb inner wrapper end -->
             </div>

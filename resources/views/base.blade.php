6d8f516e6077885901d4a76e45ea71d26a4631c3
Cybernetics <mail@cybernetics.me>
2025-10-07 11:23:23 +0530

    small updates

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -35,7 +35,7 @@
     <!-- bootstrap css -->
     <link rel="stylesheet" href="{{ asset('/assets/css/vendor/bootstrap.min.css') }}">
     <!-- main css -->
-    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css?v=1') }}">
 </head>
 
 <body class="index-twelve">
